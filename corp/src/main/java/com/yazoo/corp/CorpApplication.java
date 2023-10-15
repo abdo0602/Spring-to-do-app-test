872eba939cec74ba2f2e515dc51fd0cf67be66d4
abdo0602 <50236694+abdo0602@users.noreply.github.com>
2023-10-15 16:07:36 +0200

    Add files via upload

---
+++ corp/src/main/java/com/yazoo/corp/CorpApplication.java
@@ -0,0 +1,13 @@
+package com.yazoo.corp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorpApplication.class, args);
+	}
+
+}
