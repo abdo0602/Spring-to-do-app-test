2e4647e26ba6302dc35eb66c8871ab87d84408af
abdo0602 <50236694+abdo0602@users.noreply.github.com>
2023-10-17 00:00:41 +0200

    Update TodoController.java

--- corp/src/main/java/com/yazoo/corp/todo/TodoController.java
+++ corp/src/main/java/com/yazoo/corp/todo/TodoController.java
@@ -1,1 +1,19 @@
 package com.yazoo.corp.todo;
+imports
+
+@RestController
+public class TodoController{
+
+  private final TodoService todoService;
+  @Autowired
+  public TodoController(TodoService todoService){
+    this.todoService = todoService;
+  }
+  
+  @GetMapping
+  public List<Todo> getTodods(){
+    return todoService.getTodos();
+  }
+
+  
+}
